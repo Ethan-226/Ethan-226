a87bfc8290a267fe282b900d05b186ab45556a42
Ethan-226 <151745921+Ethan-226@users.noreply.github.com>
2023-12-07 00:54:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-const mongourl = ''; 
+const mongourl = 'mongodb+srv://admin:admin@memos.2tgoxll.mongodb.net/?retryWrites=true&w=majority'; 
 const dbName = 'test';
 
 const express = require('express');
